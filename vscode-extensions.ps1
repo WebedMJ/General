7708e0cfc1a67b362f4cd5acf4edf34668fb45a2
WebedMJ <Edward.Haynes@landmark.co.uk>
2018-07-12 10:25:24 +0100

    Code extensions

---
+++ vscode-extensions.ps1
@@ -0,0 +1,32 @@
+#code --list-extensions
+$extensions = @(
+    "DotJoshJohnson.xml",
+    "ionutvmi.reg",
+    "ms-azuretools.vscode-azureappservice",
+    "ms-azuretools.vscode-azureeventgrid",
+    "ms-azuretools.vscode-azurefunctions",
+    "ms-azuretools.vscode-azurestorage",
+    "ms-azuretools.vscode-azureterraform",
+    "ms-azuretools.vscode-cosmosdb",
+    "ms-kubernetes-tools.vscode-kubernetes-tools",
+    "ms-mssql.mssql",
+    "ms-python.python",
+    "ms-toolsai.vscode-ai",
+    "ms-vscode.azure-account",
+    "ms-vscode.azurecli",
+    "ms-vscode.PowerShell",
+    "ms-vscode.vscode-azureextensionpack",
+    "ms-vsts.team",
+    "msazurermtools.azurerm-vscode-tools",
+    "mshdinsight.azure-hdinsight",
+    "PeterJausovec.vscode-docker",
+    "redhat.vscode-yaml",
+    "Tyriar.shell-launcher",
+    "usqlextpublisher.usql-vscode-ext",
+    "VisualStudioOnlineApplicationInsights.application-insights",
+    "vsciot-vscode.azure-iot-edge",
+    "vsciot-vscode.azure-iot-toolkit",
+    "vscoss.vscode-ansible",
+    "wholroyd.jinja"
+)
+$extensions.ForEach( {code --install-extension $PSItem} )

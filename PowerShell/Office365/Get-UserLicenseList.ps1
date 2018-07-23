331ca597e6abc167ca361284524454e7ed1772bb
WebedMJ <Edward.Haynes@landmark.co.uk>
2018-07-23 12:31:37 +0100

    O365 licensed users

---
+++ PowerShell/Office365/Get-UserLicenseList.ps1
@@ -0,0 +1,15 @@
+# List licenses on each user in Azure AD
+$licensedlist = @()
+foreach ($User in (Get-MsolUser -All)) {
+    $UserInfo = Get-MsolUser -UserPrincipalName $User.UserPrincipalName
+    foreach ($license in $User.Licenses) {
+        $licensedlist += New-Object PsObject -Property @{
+            "Username"     = "$($UserInfo.DisplayName)";
+            "UPN"          = "$($UserInfo.UserPrincipalName)";
+            "AccountSKUID" = "$($license.AccountSKUid)"
+        }
+    }
+}
+$licensedlist.Count
+# Get msol user objects for licensed users...
+$licensedMsolusers = ($e3.upn).ForEach( {Get-MsolUser -UserPrincipalName $PSItem})

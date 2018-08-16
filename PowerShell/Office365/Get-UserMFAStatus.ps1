f560057e01ecf0d65d689206850cb38ef44c8dbd
WebedMJ <Edward.Haynes@landmark.co.uk>
2018-08-16 15:04:06 +0100

    adding more info

--- PowerShell/Office365/Get-UserMFAStatus.ps1
+++ PowerShell/Office365/Get-UserMFAStatus.ps1
@@ -1,9 +1,11 @@
-Get-MsolUser -all | Select-Object DisplayName, UserPrincipalName, @{
+$MFAReport = Get-MsolUser -all | Select-Object DisplayName, UserPrincipalName, BlockCredential, isLicensed, @{
     Name       = "MFA Status";
     Expression = {
         if ( $_.StrongAuthenticationRequirements.State -ne $null) {
             $_.StrongAuthenticationRequirements.State
         } else {
             "Disabled"
-        }}
-}
+        }}}, @{
+        Name       = "License";
+        Expression = {$_.Licenses.AccountSkuId}
+        }

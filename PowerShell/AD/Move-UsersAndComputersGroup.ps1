c0a363e8574b47a729234753250d9d53a54d9b5c
WebedMJ <Edward.Haynes@landmark.co.uk>
2018-08-23 12:16:44 +0100

    Better error handling

---
+++ PowerShell/AD/Move-UsersAndComputersGroup.ps1
@@ -0,0 +1,124 @@
+# Moves users from a text file list of users and SCCM associated computer account to new OU
+param (
+    [Parameter(Mandatory = $true)]$SCCMSite,
+    [Parameter(Mandatory = $true)]$ADUserGroup,
+    [Parameter(Mandatory = $true)]$NewUserOUDN,
+    [Parameter(Mandatory = $true)]$NewComputerOUDN,
+    [Parameter(Mandatory = $true)][pscredential]$ADPSCred,
+    [bool]$Confirm = $false,
+    $logfolder = $env:TEMP
+)
+$startloc = Get-Location
+$rundt = Get-Date -Format HHmmssddMMyyyy
+switch ($Confirm) {
+    $true {
+        Write-Host "Confirm set to true, moving users..."
+    }
+    $false {
+        switch (($PSBoundParameters.ContainsValue($Deploy))) {
+            $false {
+                Write-Host "Confirm not specified, default to report only..."
+            }
+            default {
+                Write-Host "Deploy set to false, running in read only mode..."
+            }
+        }
+    }
+}
+$script:WhatIfPreference = !$Confirm
+
+try {
+    Import-Module 'C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1' -ErrorAction Stop
+} catch {
+    Write-Host "Error importing SCCM module"
+    exit 1
+}
+
+# Gather users...
+Set-Location $SCCMSite":"
+$adusers = Get-ADGroup -Identity $ADUserGroup | Get-ADGroupMember | Get-ADUser -Properties *
+$userreport = @()
+$adusers.foreach( {
+        $reportline = [pscustomobject]@{
+            Username    = $_.SamAccountName
+            FullName    = $_.Name
+            CurrentPath = $_.DistinguishedName
+            NewOUDN     = $NewUserOUDN
+        }
+        $userreport += $reportline
+    })
+try {
+    $userreportpath = "$logfolder\MovedUserReport-{0}.csv" -f $rundt
+    Write-Host "Saving user report to $userreportpath"
+    $userreport | Export-Csv $userreportpath -NoTypeInformation -Force
+} catch {
+    Write-Host "Error creating user report, aborting..."
+    exit 1
+}
+
+# Gather computers...
+$alldevices = Get-CMDevice
+$userwksnames = @()
+$computerreport = @()
+$adusers.foreach( {
+        $username = $_.SamAccountName
+        $computernames = $alldevices | Where-Object {
+            $_.username -eq $username -and $_.DeviceOS -match "Workstation"
+        } |
+            Select-Object Name
+        $userwksnames += $computernames
+    })
+$adcomputers = $userwksnames.ForEach( {
+        try {
+            Get-ADComputer $_.name -ErrorAction Stop
+        } catch {
+            $message = $error[0].Exception.Message
+            Write-Host $message
+        }
+    })
+$adcomputers.foreach( {
+        $reportline = [pscustomobject]@{
+            ComputerName = $_.DNSHostName
+            CurrentPath  = $_.DistinguishedName
+            NewOUDN      = $NewComputerOUDN
+        }
+        $computerreport += $reportline
+    })
+try {
+    $computerreportpath = "$logfolder\MovedComputerReport-{0}.csv" -f $rundt
+    Write-Host "Saving user report to $computerreportpath"
+    $computerreport | Export-Csv $computerreportpath -NoTypeInformation -Force
+} catch {
+    Write-Host "Error creating computer report, aborting..."
+    exit 1
+}
+
+# Move users
+$adusers.foreach( {
+        try {
+            $un = $PSItem.Name
+            Write-Host "Moving $un"
+            $PSItem | Move-ADObject -TargetPath $NewUserOUDN -Credential $ADPSCred -PassThru -ErrorAction Stop
+        } catch {
+            $message = $error[0].Exception.Message
+            $target = $error[0].CategoryInfo.TargetName
+            Write-Host "$message on $target"
+        }
+    })
+
+# Move computers
+$adcomputers.foreach( {
+        try {
+            $cn = $PSItem.Name
+            Write-Host "Moving $cn"
+            $PSItem | Move-ADObject -TargetPath $NewComputerOUDN -Credential $ADPSCred -ErrorAction Stop
+        } catch {
+            $message = $error[0].Exception.Message
+            $target = $error[0].CategoryInfo.TargetName
+            Write-Host "$message on $target"
+        }
+    })
+
+Set-Location $startloc
+$openlogfolder = "explorer $logfolder"
+Invoke-Expression $openlogfolder
--- PowerShell/AD/Move-UsersAndComputersList.ps1
+++ PowerShell/AD/Move-UsersAndComputersList.ps1
@@ -1,13 +1,14 @@
 # Moves users from AD Group and and SCCM associated computer account to new OU
 param (
-    $SCCMSite,
-    [Parameter(Mandatory = $true)]$ADUserGroup,
-    $NewUserOUDN,
-    $NewComputerOUDN,
+    [Parameter(Mandatory = $true)]$SCCMSite,
+    [Parameter(Mandatory = $true)]$UserListFilePath,
+    [Parameter(Mandatory = $true)]$NewUserOUDN,
+    [Parameter(Mandatory = $true)]$NewComputerOUDN,
     [Parameter(Mandatory = $true)][pscredential]$ADPSCred,
     [bool]$Confirm = $false,
     $logfolder = $env:TEMP
 )
+$startloc = Get-Location
 $rundt = Get-Date -Format HHmmssddMMyyyy
 switch ($Confirm) {
     $true {
@@ -35,7 +36,13 @@ try {
 
 # Gather users...
 Set-Location $SCCMSite":"
-$adusers = Get-ADGroup -Identity $ADUserGroup | Get-ADGroupMember | Get-ADUser -Properties *
+try {
+    $userlist = Get-Content -Path $UserListFilePath -ErrorAction Stop
+} catch {
+    Write-Host "Error getting user list"
+    exit 1
+}
+$adusers = $userlist | Get-ADUser -ErrorAction SilentlyContinue
 $userreport = @()
 $adusers.foreach( {
         $reportline = [pscustomobject]@{
@@ -67,7 +74,14 @@ $adusers.foreach( {
             Select-Object Name
         $userwksnames += $computernames
     })
-$adcomputers = $userwksnames.ForEach( {Get-ADComputer $_.name})
+$adcomputers = $userwksnames.ForEach( {
+        try {
+            Get-ADComputer $_.name -ErrorAction Stop
+        } catch {
+            $message = $error[0].Exception.Message
+            Write-Host $message
+        }
+    })
 $adcomputers.foreach( {
         $reportline = [pscustomobject]@{
             ComputerName = $_.DNSHostName
@@ -86,28 +100,31 @@ try {
 }
 
 # Move users
-try {
-    $adusers.foreach( {
+$adusers.foreach( {
+        try {
             $un = $PSItem.Name
             Write-Host "Moving $un"
             $PSItem | Move-ADObject -TargetPath $NewUserOUDN -Credential $ADPSCred -PassThru -ErrorAction Stop
-        })
-} catch {
-    $message = $error[0].Exception.Message
-    Write-Host $message
-}
+        } catch {
+            $message = $error[0].Exception.Message
+            $target = $error[0].CategoryInfo.TargetName
+            Write-Host "$message on $target"
+        }
+    })
 
 # Move computers
-try {
-    $adcomputers.foreach( {
+$adcomputers.foreach( {
+        try {
             $cn = $PSItem.Name
             Write-Host "Moving $cn"
             $PSItem | Move-ADObject -TargetPath $NewComputerOUDN -Credential $ADPSCred -ErrorAction Stop
-        })
-}
-catch {
-    $message = $error[0].Exception.Message
-    Write-Host $message
-}
+        } catch {
+            $message = $error[0].Exception.Message
+            $target = $error[0].CategoryInfo.TargetName
+            Write-Host "$message on $target"
+        }
+    })
+
+Set-Location $startloc
 $openlogfolder = "explorer $logfolder"
 Invoke-Expression $openlogfolder

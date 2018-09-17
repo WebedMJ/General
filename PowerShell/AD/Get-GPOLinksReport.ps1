64c707f682dda63b15ecb9949fac51bcb0696e96
WebedMJ <Edward.Haynes@landmark.co.uk>
2018-09-17 11:53:37 +0100

    get-gpolinksreport initial commit

---
+++ PowerShell/AD/Get-GPOLinksReport.ps1
@@ -0,0 +1,13 @@
+[xml]$gporeport = Get-GPOReport -All -ReportType Xml
+$gpolinks = ($gporeport.gpos.gpo).foreach( {
+        [PSCustomObject]@{
+            Name    = $PSItem.Name
+            LinksTo = $PSItem.LinksTo.SOMPath
+        }
+    })
+$gpolinks
+
+# Remove unlinked GPOs
+# $gpolinks.Where( {!$PSItem.LinksTo}) # Misses site linked GPOs!
+$gpostoremove = $gpolinks.Where( {(!$PSItem.LinksTo) -and ($PSItem.Name -ne 'SomeGPOLinkedToASite')})
+$gpostoremove.Name.ForEach( {Get-GPO -Name $PSItem | Remove-GPO})

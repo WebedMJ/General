36163aeb43f3b6f72c581373c755f1742ca83ad1
webedmj <23118000+WebedMJ@users.noreply.github.com>
2019-03-23 16:51:38 +0000

    Adding MSGraph, AADGraph, sharedkey

--- PowerShell/Azure/Modules/AzureRESTAuth/AzureRESTAuth.psm1
+++ PowerShell/Azure/Modules/AzureRESTAuth/AzureRESTAuth.psm1
@@ -15,73 +15,166 @@ function Get-AzureRESTtoken {
     <#
     .SYNOPSIS
         Uses web/function app's Azure AD managed identity or Azure Automation RunAs to get Azure REST API tokens.
+        Can also use AAD client id / secret for shared key authorization with MSGraph and ARM.
+
+        Returns a hashtable with authorization (with bearer token) and contentType to use as an authorization header.
     .PARAMETER AzureResource
-        Choose which azure resource to get an authorization token for when using managed identities.
-        No effect when using Azure Automation RunAs.
+        Choose which azure resource to get an authorization token for when using Managed Identity.
+        No effect when using Azure Automation RunAs or Shared Key.
 
-        Valid values: ARM, Vault
+        Valid values: ARM, Vault, MSGraph, AADGraph
     .PARAMETER AzureIdentity
-        Select which authentication source to use, currently supports managed identities and Azure Automation RunAs accounts.
+        Select which authentication source to use, currently supports managed identities, Azure Automation RunAs accounts,
+        and Shared Key.
 
-        Valid values: ManagedIdentity, AzureAutomationRunAs
+        Valid values: ManagedIdentity, AzureAutomationRunAs, SharedKey
+    .PARAMETER SharedKeyScope
+        For use with SharedKey authorization with.
+        Select the scope to authorize with the shared client id and secret key.
+    .PARAMETER ClientId
+        Client Id aor Application Id from an AAD appliction registration.
+    .PARAMETER ClientSecret
+        Generated Secret from the AAD application registration referenced by ClientId
+    .PARAMETER AADTenantDomain
+        Domain name of the AAD tenant for the registered AAD application.
+        In the form of domain.onmicrosoft.com.
     .LINK
         https://github.com/WebedMJ/General/tree/master/PowerShell/Azure/Modules/AzureRESTAuth
     #>
     [CMDLetBinding()]
+    [OutputType("System.Collections.Hashtable")]
     param (
+        [Parameter(Mandatory = $false, ParameterSetName = "ManagedIdentity")]
+        [ValidateSet("ARM", "Vault", "MSGraph", "AADGraph")]
+        [ValidateCount(1, 1)]
+        [string[]]$AzureResource = 'ARM',
         [Parameter(Mandatory = $false)]
-        [ValidateSet("ARM", "Vault")]
-        [string]$AzureResource = 'ARM',
-        [Parameter(Mandatory = $false)]
-        [ValidateSet("ManagedIdentity", "AzureAutomationRunAs")]
-        [string]$AzureIdentity = 'ManagedIdentity'
+        [ValidateSet("ManagedIdentity", "AzureAutomationRunAs", "SharedKey")]
+        [ValidateCount(1, 1)]
+        [string[]]$AzureIdentity = 'ManagedIdentity',
+        [Parameter(Mandatory = $true, ParameterSetName = "SharedKey",
+            HelpMessage = "Valid values: MSGraph, ARM.  Maximum of one allowed.")]
+        [ValidateSet("MSGraph", "ARM")]
+        [ValidateCount(1, 1)]
+        [string[]]$SharedKeyScope,
+        [Parameter(Mandatory = $true, ParameterSetName = "SharedKey")]
+        [string]$ClientId,
+        [Parameter(Mandatory = $true, ParameterSetName = "SharedKey")]
+        [string]$ClientSecret,
+        [Parameter(Mandatory = $true, ParameterSetName = "SharedKey")]
+        [string]$AADTenantDomain
     )
-    switch ($AzureIdentity) {
-        'ManagedIdentity' {
-            $apiVersion = '2017-09-01'
-            switch ($AzureResource) {
-                'ARM' { $resourceURI = 'https://management.azure.com' }
-                'Vault' { $resourceURI = 'https://vault.azure.net' }
+
+    begin {
+        $funcerror = $null
+        $ARMuri = 'https://management.azure.com'
+        $Vaulturi = 'https://vault.azure.net'
+        $MSGraphuri = 'https://graph.microsoft.com'
+        $AADGraphuri = 'https://graph.windows.net'
+        switch ($AzureIdentity) {
+            'ManagedIdentity' {
+                $apiVersion = '2017-09-01'
+                switch ($AzureResource) {
+                    'ARM' { $resourceURI = $ARMuri }
+                    'Vault' { $resourceURI = $Vaulturi }
+                    'MSGraph' { $resourceURI = $MSGraphuri }
+                    'AADGraph' { $resourceURI = $AADGraphuri }
+                }
+                $uriquery = '?resource={0}&api-version={1}' -f $resourceURI, $apiVersion
+                [uri]$uriendpoint = $env:MSI_ENDPOINT
+                $uri = [System.UriBuilder]::new(
+                    $uriendpoint.Scheme,
+                    $uriendpoint.Host,
+                    $uriendpoint.Port,
+                    $uriendpoint.AbsolutePath,
+                    $uriquery
+                )
+                $tokenheaders = @{
+                    Secret = $env:MSI_SECRET
+                }
+                $tokensplat = @{
+                    Method  = 'Get'
+                    Headers = $tokenheaders
+                    Uri     = $uri.Uri
+                }
             }
-            $uriquery = '?resource={0}&api-version={1}' -f $resourceURI, $apiVersion
-            [uri]$uriendpoint = $env:MSI_ENDPOINT
-            $uri = [System.UriBuilder]::new(
-                $uriendpoint.Scheme,
-                $uriendpoint.Host,
-                $uriendpoint.Port,
-                $uriendpoint.AbsolutePath,
-                $uriquery
-            )
-            $tokenheaders = @{
-                Secret = $env:MSI_SECRET
+            'AzureAutomationRunAs' {
+                $AzRunAs = Get-AutomationConnection -Name AzureRunAsConnection
+                $Azlogin = @{
+                    ServicePrincipal      = $true
+                    Tenant                = $AzRunAs.TenantId
+                    ApplicationId         = $AzRunAs.ApplicationId
+                    CertificateThumbprint = $AzRunAs.CertificateThumbprint
+                }
             }
-            $tokensplat = @{
-                Method  = 'Get'
-                Headers = $tokenheaders
-                Uri     = $uri.Uri
+            'SharedKey' {
+                If (!($PSBoundParameters.ContainsKey('ClientId'))) {
+                    throw "Error: ClientId, ClientSecret and AADTenantDomain are required when using SharedKey"
+                }
+                $loginurl = 'https://login.microsoftonline.com'
+                switch ($SharedKeyScope) {
+                    'ARM' { $SKScopeURI = $ARMuri }
+                    'MSGraph' { $SKScopeURI = "$MSGraphuri/.default" }
+                }
+                $tokenbody = @{
+                    client_id     = $ClientId
+                    scope         = $SKScopeURI
+                    client_secret = $ClientSecret
+                    grant_type    = 'client_credentials'
+                }
+                $tokensplat = @{
+                    Uri         = "$loginurl/$AADTenantDomain/oauth2/v2.0/token"
+                    Method      = 'Post'
+                    Body        = $tokenbody
+                    ErrorAction = 'Stop'
+                }
             }
-            $tokenResponse = Invoke-RestMethod @tokensplat
-            $accessToken = $tokenResponse.access_token
-        }
-        'AzureAutomationRunAs' {
-            $AzRunAs = Get-AutomationConnection -Name AzureRunAsConnection
-            $Azlogin = @{
-                ServicePrincipal      = $true
-                Tenant                = $AzRunAs.TenantId
-                ApplicationId         = $AzRunAs.ApplicationId
-                CertificateThumbprint = $AzRunAs.CertificateThumbprint
-            }
-            $Azsession = Login-AzureRmAccount @Azlogin
-            $context = Get-AzureRmContext
-            $SubscriptionId = $context.SubscriptionId
-            $cache = $context.TokenCache
-            $cacheItems = $cache.ReadItems()
-            $accesstoken = $cacheItems[$cacheItems.Count - 1].AccessToken
         }
     }
-    $tokenheader = @{
-        Authorization  = "Bearer $accesstoken"
-        'Content-Type' = 'application/json'
+
+    process {
+        switch ($AzureIdentity) {
+            'ManagedIdentity' {
+                try {
+                    $tokenResponse = Invoke-RestMethod @tokensplat
+                    $accessToken = $tokenResponse.access_token
+                } catch {
+                    $funcerror = $Error[0].Exception
+                }
+            }
+            'AzureAutomationRunAs' {
+                try {
+                    $Azsession = Login-AzureRmAccount @Azlogin
+                    $context = Get-AzureRmContext
+                    $SubscriptionId = $context.SubscriptionId
+                    $cache = $context.TokenCache
+                    $cacheItems = $cache.ReadItems()
+                    $accesstoken = $cacheItems[$cacheItems.Count - 1].AccessToken
+                } catch {
+                    $funcerror = $Error[0].Exception
+                }
+            }
+            'SharedKey' {
+                try {
+                    $tokenResponse = Invoke-RestMethod @tokensplat
+                    $accessToken = $tokenResponse.access_token
+                } catch {
+                    $funcerror = $Error[0].Exception
+                }
+            }
+        }
     }
-    return $tokenheader
+
+    end {
+        if (!$funcerror) {
+            $tokenheader = @{
+                Authorization  = "Bearer $accesstoken"
+                'Content-Type' = 'application/json'
+            }
+            return $tokenheader
+        } else {
+            throw $funcerror
+        }
+    }
+
 }

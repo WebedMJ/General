d4e116d1d56d38e7ccff7dc00e2849abbe60cc2e
Ed Haynes <23118000+WebedMJ@users.noreply.github.com>
2019-03-26 13:52:15 +0000

    oh-my-posh

---
+++ oh-my-posh/oh-my-posh_config.ps1
@@ -0,0 +1,22 @@
+# See https://github.com/TylerLeonhardt/oh-my-posh
+# Install https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/Meslo/L/Regular/complete
+
+# Install-Module posh-git -Scope CurrentUser
+Install-Module oh-my-posh -Scope CurrentUser
+
+# In C:\tools\Cmder\config\user_profile.ps1
+[ScriptBlock]$CmderPrompt = {
+    Import-Module posh-git
+    Import-Module oh-my-posh
+    $ohmyposhthemedir = 'C:\Users\EHaynes\OneDrive - Landmark Information Group Ltd\Documents\PowerShell\PoshThemes'
+    if (!(Test-Path $ohmyposhthemedir)) {
+        New-Item -Path $ohmyposhthemedir -ItemType Directory
+    }
+    $global:ThemeSettings.MyThemesLocation = $ohmyposhthemedir
+    Set-Theme Paradox-Ed
+}
+
+
+# Copy custome theme psm1 file to mythemeslocation...
+# Use Monokai ConEmu theme
+# Use MesloLGL NF main console font in ConEmu

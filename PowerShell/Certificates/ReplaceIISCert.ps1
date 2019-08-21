6438f09cff41ba1ad648162af6f87e8b81fabeb5
Ed Haynes <23118000+WebedMJ@users.noreply.github.com>
2019-08-21 11:52:01 +0100

    Tweaks

--- PowerShell/Certificates/ReplaceIISCert.ps1
+++ PowerShell/Certificates/ReplaceIISCert.ps1
@@ -1,21 +1,15 @@
 $servername = 'server01'
 $certfile = 'C:\certs\webcert.pfx'
 $dollarcred = Get-Credential -Message "Enter server admin account"
-$z2mediaweb01 = New-PSSession $servername -Credential $dollarcred
-Invoke-Command -ComputerName $servername -Credential $dollarcred -ScriptBlock {
-    New-Item C:\Files -Type Directory -ErrorAction SilentlyContinue
+$certpassword = Read-Host -Prompt "PFX Password" -AsSecureString
+$newcertsplat = @{
+    Password          = $certpassword
+    CertStoreLocation = 'Cert:\LocalMachine\My'
+    FilePath          = $certfile
 }
-Copy-Item $certfile 'C:\Files\landmark.pfx' -Force -ToSession $z2mediaweb01
-Invoke-Command -ComputerName $servername -Credential $dollarcred -ScriptBlock {
-    $certpassword = Read-Host -Prompt "PFX Password" -AsSecureString
-    $newcertsplat = @{
-        Password          = $certpassword
-        CertStoreLocation = 'Cert:\LocalMachine\My'
-        FilePath          = 'C:\certs\webcert.pfx'
-    }
-    $newcert = Import-PfxCertificate @newcertsplat
-    if ($null -ne $newcert.Thumbprint) {
-        Remove-Item $($newcertsplat.FilePath) -Force
-    }
-    Get-Item Cert:\LocalMachine\My\$($newcert.Thumbprint) | Set-Item IIS:\SslBindings\0.0.0.0!443
+$newcert = Import-PfxCertificate @newcertsplat
+if ($null -ne $newcert.Thumbprint) {
+    Remove-Item $($newcertsplat.FilePath) -Force
 }
+Remove-Item IIS:\SslBindings\0.0.0.0!443
+Get-Item Cert:\LocalMachine\My\$($newcert.Thumbprint) | New-Item IIS:\SslBindings\0.0.0.0!443

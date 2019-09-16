57c4815670b675c996f62e75f1c52fe7ee3cfaca
Ed Haynes <23118000+WebedMJ@users.noreply.github.com>
2019-09-16 11:27:06 +0100

    credit

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,3 +1,4 @@
+# https://twitter.com/lee_holmes/status/1172640465767682048?s=12
 Set-PSReadLineOption -AddToHistoryHandler {
     param([string]$line)
     $sensitive = "password|asplaintext|token|key|secret"
